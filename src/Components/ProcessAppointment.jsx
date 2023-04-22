efa7c136fc739def481326cd4b2eaa1b171a9ad2
nitesh.kumar <nkumar@4321#>
2023-04-22 12:07:46 +0530

    dashboard

--- src/Components/ProcessAppointment.jsx
+++ src/Components/ProcessAppointment.jsx
@@ -1,7 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import { getAllHospitalsForOrg } from '../Service/commonService';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+        '& tbody tr:hover': {
+          backgroundColor: '#f6fff2',
+        },
+      },
+      tableHead: {
+        backgroundColor: '#f6fff2',
+        fontWeight: 'bold',
+        fontSize: '24px',
+      },
+      listContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        marginTop: '50px',
+      },
+      listTitle: {
+        fontWeight: 'bold',
+        fontSize: '32px',
+        marginBottom: '20px',
+      },
+
+})
+
 
 const ProcessAppointment = (props) =>{
+    const classes = useStyles();
     const [hospitals, sethospitals] = useState([]);
     React.useEffect(()=>{
         async function getAllHospitals() {
@@ -19,9 +50,39 @@ const ProcessAppointment = (props) =>{
         getAllHospitals();
       }, []);
 
+const handleAddappointment = (hospialEmail)=>{
+
+    
+}
     return(
         <>
-            OKKKKKKKKKKKKKKK
+          <div className={classes.listContainer}>
+        <Typography variant="subtitle1" className={classes.listTitle} style={{fontSize:'32px'}}>
+          List of Available Hospitals
+        </Typography>
+      </div>
+          <TableContainer component={Paper} style={{ marginTop: '30px', width: '80%' ,margin: 'auto' }}>
+          <Table className={classes.table}>
+          <TableHead>
+            <TableRow className={classes.tableHead}>
+            <TableCell sx={{ fontWeight: 'bold' ,fontSize: '20px' }}>hospial Email</TableCell>
+              <TableCell sx={{ fontWeight: 'bold' ,fontSize: '20px' }}>hospital Name</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {hospitals.map((hospital) =>(
+                <>
+                <TableRow key={hospital.hospitalId}>
+                <TableCell style={{ fontWeight: 'bold', fontSize: '15px' }}><Button variant="text"  sx={{ textTransform: 'none' }} >{hospital.hospitalEmail}</Button></TableCell>
+                <TableCell style={{ fontWeight: 'bold', fontSize: '15px' }}>{hospital.hospitalName}</TableCell>
+                <TableCell><Button variant="contained" onClick={() => handleAddappointment(hospital.hospitalEmail)}>Book</Button></TableCell>
+              </TableRow>
+                </>
+            ))}
+          </TableBody>
+         </Table>
+          </TableContainer>
         </>
     )
 }
