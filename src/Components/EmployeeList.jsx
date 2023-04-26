1d38dee6d41d6973b88eb9971fb22e99db7c42b1
nitesh.kumar <nkumar@4321#>
2023-04-26 21:19:55 +0530

    changes

--- src/Components/EmployeeList.jsx
+++ src/Components/EmployeeList.jsx
@@ -2,6 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Alert, AlertTitle, Button, Dialog, Paper, Table, TableBody,TablePagination, TableCell, TableContainer, TableHead, TableRow, Typography, CircularProgress } from "@mui/material";
 import React, { useState } from "react";
 import { getEmpForOrg } from '../Service/EmployeeService';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const useStyles = makeStyles({
     table: {
@@ -41,6 +42,7 @@ const EmployeeList  =  (props) =>{
     React.useEffect(()=>{
         async function fetchData() {
     const emps = await getEmpForOrg(props.orgEmail);
+    console.log(emps);
     if(emps!=null){
             setEmps(emps);
             setIsLoading(false);
@@ -70,11 +72,15 @@ const EmployeeList  =  (props) =>{
                 <TableHead>
                   <TableRow className={classes.tableHead} style={{whiteSpace: 'nowrap'}}>
                   <TableCell sx={{ fontWeight: 'bold' ,fontSize: '18px' }}> #Id</TableCell>
+                  <TableCell sx={{ fontWeight: 'bold' ,fontSize: '18px' }}> Name</TableCell>
                     <TableCell sx={{ fontWeight: 'bold' ,fontSize: '18px' }}> Email</TableCell>
                     {/* <TableCell sx={{ fontWeight: 'bold' ,fontSize: '18px' }}>claim date</TableCell>
                     <TableCell sx={{ fontWeight: 'bold' ,fontSize: '18px' }}>status</TableCell>
                     <TableCell sx={{ fontWeight: 'bold' ,fontSize: '18px' }}>remarks</TableCell> */}
-                    {/* <TableCell>Amount</TableCell> <TableCell></TableCell> */}
+                    <TableCell  sx={{ fontWeight: 'bold' ,fontSize: '18px' }}>Last Checkup date</TableCell>
+                     <TableCell  sx={{ fontWeight: 'bold' ,fontSize: '18px' }}>Date of Birth</TableCell>
+                     <TableCell  sx={{ fontWeight: 'bold' ,fontSize: '18px' }}>Address</TableCell>
+                     <TableCell></TableCell>
                     </TableRow>
                     </TableHead>
                     <TableBody>
@@ -85,8 +91,13 @@ const EmployeeList  =  (props) =>{
                       <>
                       <TableRow key={employee.employeeId} style={{height:'5px', whiteSpace: 'nowrap'}}>
                       <TableCell style={{ fontSize: '15px'}}>{employee.employeeId}</TableCell>
-                      {/* <TableCell style={{ fontWeight: 'bold', fontSize: '15px'}}><Button variant="text"  sx={{ textTransform: 'none' }} >{claim.agencyEmail}</Button></TableCell> */}
-                      <TableCell style={{ fontWeight: 'bold', fontSize: '15px'}}>{employee.empEmail}</TableCell>
+                      <TableCell style={{ fontSize: '15px'}}>{employee.empName}</TableCell>
+                      <TableCell style={{fontSize: '15px'}}>{employee.empEmail}</TableCell>
+                      <TableCell style={{fontSize: '15px'}}>{employee.lastCheckupDate}</TableCell>
+                      <TableCell style={{fontSize: '15px'}}>{employee.dob}</TableCell>
+                      <TableCell style={{fontSize: '15px',wordWrap: 'break-word'}}>{employee.addLine1} <br/>{employee.city} {employee.zip}</TableCell>
+                      <TableCell> <Button ><DeleteIcon sx={{ color: 'red' }} /></Button></TableCell>
+
                       {/* <TableCell style={{  fontSize: '15px'}}>{claim.claimDate}</TableCell> 
                       <TableCell style={{  fontSize: '15px'}}>{claim.status}</TableCell>  */}
                       </TableRow>
